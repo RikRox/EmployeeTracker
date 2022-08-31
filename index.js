8b5ce436d47362c1d8361de395e98cb83a28c04b
RikRox <104681617+RikRox@users.noreply.github.com>
2022-08-31 12:35:00 -0400

    update for user input

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-const inquirer = import('inquirer');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+//require('console.table');
 
-// function optionPrompt() { 
-//     inquirer
-//         .prompt
-//         ({
-//             type: "list",
-//             name: "option",
-//             message: "What would you like to do?",
-//             choices: [
-//                 "View Employees",
-//                 "View Departments",
-//                 "View Roles",
-//                 "Add Employee",
-//                 "Add Department",
-//                 "Add Role",
-//                 "Update Employee Role"
-//             ]
-//         }).then((answer) => {
-//             if(answer == "View Employees"){
-//                 viewEmployees();
-//             } else if (answer == "View Departments"){
-//                 viewDepartments();
-//             } else if (answer == "View Roles"){
-//                 viewRoles();
-//             } else if (answer == "Add Employee"){
-//                 addEmployee();
-//             } else if (answer == "Add Department"){
-//                 addDepartment();
-//             } else if (answer == "Add Role"){
-//                 addRole();
-//             }
-//         });
-// };
+//sql connection
+const db = require('./db/connection');
+
+db.connect(err => {
+  if (err) throw err; 
+  console.log('DATABASE CONNECTED');
+
+      optionPrompt();
+  });
 
 
-function test() {
-    console.log("is it working?");
-}
 
-//optionPrompt;
 
-module.exports = {optionPrompt};
+
+
+// prompt user inputs
+  function optionPrompt() { 
+    inquirer
+        .prompt({
+            type: "list",
+            name: "option",
+            message: "What would you like to do?",
+            choices: [
+                "View Employees",
+                "View Departments",
+                "View Roles",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "Update Employee Role"
+            ]
+        }).then((answer) => {
+            if(answer == "View Employees"){
+                viewEmployees();
+            } else if (answer == "View Departments"){
+                viewDepartments();
+            } else if (answer == "View Roles"){
+                viewRoles();
+            } else if (answer == "Add Employee"){
+                addEmployee();
+            } else if (answer == "Add Department"){
+                addDepartment();
+            } else if (answer == "Add Role"){
+                addRole();
+            }
+        });
+};
+
+function viewEmployees(){};
